39cbf7d8745562f1e18a3ae9eb0f0869ec132dbb
Indrawan <indrawanismail0@gmail.com>
2025-07-27 14:30:46 +0800

    feat: react-confetti when win

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -10,9 +10,11 @@ import img8 from "../assets/images/anime-8.jpg"
 import img9 from "../assets/images/anime-9.jpg"
 import audio from "../assets/sound/loveLori.mp3"
 import 'primeicons/primeicons.css'
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
 
 // sisa task 
-// responsive btn  pakai xs:
+// responsive btn  pakai xs:✅
 // atur confetti
 // set game lose when timelimit
 
@@ -26,11 +28,12 @@ export default function Board({ onBack }) {
     const [isShuffling, setIsShuffling] = useState(true)
     const pieceImage = [img1, img2, img3, img4, img5, img6, img7, img8, img9]
     const timerRef = useRef(null)
+    const { width, height } = useWindowSize()
 
     useEffect(() => {
         setTiles([0, 1, 2, 3, 4, 5, 6, 7, 8])
         setIsEmpty(8)
-        
+
         const shuffleTimer = setTimeout(() => {
             initializePuzzle()
             startGameTimer()
@@ -66,10 +69,10 @@ export default function Board({ onBack }) {
     const initializePuzzle = () => {
         const initialTiles = [0, 1, 2, 3, 4, 5, 6, 7, 8]
         const shuffled = [...initialTiles]
-        
+
         for (let i = shuffled.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1))
-            ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+                ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
         }
 
         const inversions = countInversions(shuffled)
@@ -80,7 +83,7 @@ export default function Board({ onBack }) {
                 ;[shuffled[2], shuffled[3]] = [shuffled[3], shuffled[2]]
             }
         }
-        
+
         setTiles(shuffled)
         setIsEmpty(shuffled.indexOf(8))
         setMoves(0)
@@ -106,7 +109,7 @@ export default function Board({ onBack }) {
     }
 
     const handleTileClick = (index) => {
-        if (isWon || isLose ||timeLeft === 0 || isShuffling) return
+        if (isWon || isLose || timeLeft === 0 || isShuffling) return
 
         const clickedRow = Math.floor(index / 3)
         const clickedCol = index % 3
@@ -119,8 +122,8 @@ export default function Board({ onBack }) {
 
         if (isAdjacent) {
             const newTiles = [...tiles]
-            ;[newTiles[index], newTiles[isEmpty]] = [newTiles[isEmpty], newTiles[index]]
-            
+                ;[newTiles[index], newTiles[isEmpty]] = [newTiles[isEmpty], newTiles[index]]
+
             setTiles(newTiles)
             setIsEmpty(index)
             setMoves(moves + 1)
@@ -135,64 +138,67 @@ export default function Board({ onBack }) {
     return (
         <section className="container grid w-full min-h-full justify-center items-center text-shadow-white p-32">
             {audio && <audio src={audio} autoPlay loop></audio>}
-                <div className="text-center mb-2">
-                    <p className="text-white">
-                        {isShuffling 
-                            ? "Mengacak gambar..." 
-                            : isLose ? "Anda kalah" : 
-                            isWon 
-                                ? "Sn[[nelamat! Anda menang!" 
+            {isWon && <Confetti
+                width={width}
+                height={height}
+            />}
+            <div className="text-center mb-2">
+                <p className="text-white">
+                    {isShuffling
+                        ? "Mengacak gambar..."
+                        : isLose ? "Anda kalah" :
+                            isWon
+                                ? "Selamat! Anda menang!"
                                 : "Selesaikan puzzle!"}
-                    </p>
-                </div>
-                
-                <div className="flex justify-between items-center mb-3">
-                    <div className="flex items-center gap-1 md:gap-3">
-                        <i className="pi pi-clock text-white"></i>
-                        <p className="font-normal text-white">{formatTime()}</p>
-                    </div>
+                </p>
+            </div>
 
-                    <div className="flex justify-center items-center">
-                        <button
-                            onClick={onBack}
-                            className="flex items-center gap-1 md:gap-2 cursor-pointer border-3 border-[#4b525a] hover:bg-[#4b525a] bg-[#1f2226]  min-w-18 max-w-22 max-h-10 font-stretch-ultra-condensed text-white rounded-lg transition-colors px-3 text-center"
-                        >
-                            <i className="pi pi-arrow-left bounceHorizontalLeft text-center" style={{ fontSize: '0.800rem', color:"white" }}></i>
-                            reset
-                        </button>
-                    </div>
+            <div className="flex justify-between items-center mb-3">
+                <div className="flex items-center gap-1 md:gap-3">
+                    <i className="pi pi-clock text-white"></i>
+                    <p className="font-normal text-white">{formatTime()}</p>
                 </div>
+
+                <div className="flex justify-center items-center">
+                    <button
+                        onClick={onBack}
+                        className="flex items-center gap-1 md:gap-2 cursor-pointer border-3 border-[#4b525a] hover:bg-[#4b525a] bg-[#1f2226]  min-w-18 max-w-22 max-h-10 font-stretch-ultra-condensed text-white rounded-lg transition-colors px-3 text-center"
+                    >
+                        <i className="pi pi-arrow-left bounceHorizontalLeft text-center" style={{ fontSize: '0.800rem', color: "white" }}></i>
+                        reset
+                    </button>
+                </div>
+            </div>
 
             {(isWon || isLose) && (
-                    <div>
-                        <p className="text-xl font-bold text-white text-center">{isWon ? '🎉 Congrats!' : ' Game Over!'}</p>
-                        <p className="text-white text-center mb-2">{isWon ? `Kamu menyelesaikan puzzle dalam ${moves} langkah!` : 'Waktu habis! Coba lagi.'}</p>
-                    </div>
-                )}
-
-                <div className={`grid grid-cols-3 w-86 md:gap-1 md:w-96 md:h-96 ${isShuffling ? 'opacity-70' : ''}`}>
-                    {tiles.map((tile, index) => (
-                        <div
-                            key={index}
-                            onClick={() => handleTileClick(index)}
-                            className={`flex justify-center items-center overflow-hidden transition-all ${
-                                tile === 8
-                                    ? 'bg-gray-900 cursor-not-allowed'
-                                    : 'bg-gray-700 shadow-lg hover:shadow-xl cursor-pointer'
-                            } ${isShuffling ? 'pointer-events-none' : ''}`}
-                        >
-                            {tile !== 8 && (
-                                <img
-                                    src={pieceImage[tile]}
-                                    alt={`Piece ${tile + 1}`}
-                                    className="w-full h-full object-cover"
-                                    draggable={false}
-                                />
-                            )}
-                        </div>
-                    ))}
+                <div>
+                    <p className="text-xl font-bold text-white text-center">{isWon ? '🎉 Congrats!' : ' Game Over!'}</p>
+                    <p className="text-white text-center mb-2">{isWon ? `Kamu menyelesaikan puzzle dalam ${moves} langkah!` : 'Waktu habis! Coba lagi.'}</p>
                 </div>
-            
+            )}
+
+            <div className={`grid grid-cols-3 w-86 md:gap-1 md:w-96 md:h-96 ${isShuffling ? 'opacity-70' : ''}`}>
+                {tiles.map((tile, index) => (
+                    <div
+                        key={index}
+                        onClick={() => handleTileClick(index)}
+                        className={`flex justify-center items-center overflow-hidden transition-all ${tile === 8
+                                ? 'bg-gray-900 cursor-not-allowed'
+                                : 'bg-gray-700 shadow-lg hover:shadow-xl cursor-pointer'
+                            } ${isShuffling ? 'pointer-events-none' : ''}`}
+                    >
+                        {tile !== 8 && (
+                            <img
+                                src={pieceImage[tile]}
+                                alt={`Piece ${tile + 1}`}
+                                className="w-full h-full object-cover"
+                                draggable={false}
+                            />
+                        )}
+                    </div>
+                ))}
+            </div>
+
         </section>
     )
 }
